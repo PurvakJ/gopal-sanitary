2a485be2d1c4ebf2609d8e2eb80c08ae933d80e1
Purvak Jindal <137681540+PurvakJ@users.noreply.github.com>
2026-05-26 13:39:14 +0530

    changes

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -104,7 +104,7 @@ function ContactPage() {
   const whatsappMessage = encodeURIComponent('Hello, I would like to know more about your products');
 
   const contactInfo = [
-    { icon: <FaMapMarkerAlt />, title: 'Visit Our Store', details: ['GOPAL SANITARY HOUSE', 'Near Bus Stand, Main Road', 'City - 123456, India'] },
+    { icon: <FaMapMarkerAlt />, title: 'Visit Our Store', details: ['GOPAL SANITARY HOUSE', 'Near Ganga Oil Mill, J.K. Road', 'Mansa - 151505'] },
     { icon: <FaPhone />, title: 'Call Us', details: ['+91 90562 62171', '+91 99888 83123'], links: ['tel:+919056262171', 'tel:+919988883123'] },
     { icon: <FaEnvelope />, title: 'Email Us', details: ['gopalsanitaryhousemansa@gmail.com'], links: ['mailto:gopalsanitaryhousemansa@gmail.com'] },
     { icon: <FaClock />, title: 'Business Hours', details: ['Monday - Saturday: 9:00 AM - 8:00 PM', 'Sunday: 10:00 AM - 2:00 PM'] },
